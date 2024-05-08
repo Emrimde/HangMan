911f5e42c589479d54575f9a14eb462d607955b1
Admin <kkrzysztofgargas@gmail.com>
2024-05-08 16:06:00 +0200

    Start window styled, components added

--- src/HangManClient/HangMan.java
+++ src/HangManClient/HangMan.java
@@ -1,14 +1,54 @@
 package HangManClient;
 
+import java.awt.*;
+import java.awt.Dimension;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class HangMan extends JFrame {
-
+    
     public HangMan() {
+        ImageIcon cloud1 = new ImageIcon("cloud1.png");
+        Font mvBoli = new Font("MV Boli", Font.BOLD,35); 
+        JButton startButton = new JButton("Start");
+        JPanel subPanel = new JPanel();
+        JPanel bottomPanel = new JPanel();
+        JPanel upperPanel = new JPanel();
+         
+        startButton.setFont(mvBoli);
+        startButton.setFocusable(false);
+        startButton.setBackground(new Color(21, 194, 82));
+        startButton.setForeground(Color.yellow);
+        startButton.setBorder(BorderFactory.createLineBorder(Color.yellow, 5, true));
+        JLabel label = new JLabel("HangMan");
+        JLabel cloudLabel1 = new JLabel();
+        cloudLabel1.setIcon(cloud1);
+        JLabel cloudLabel2 = new JLabel();
+        cloudLabel2.setIcon(cloud1);
+        label.setFont(mvBoli);
+        label.setForeground(new Color(247, 240, 15));
+        
+        subPanel.setPreferredSize(new Dimension(200,50));
+        subPanel.setLayout(new BorderLayout());
+        subPanel.add(startButton, BorderLayout.CENTER);
+       
+        bottomPanel.setBackground(new Color(21, 194, 82));
+        bottomPanel.setPreferredSize(new Dimension(500,200));
+        bottomPanel.setLayout(new FlowLayout(FlowLayout.CENTER,0,30));
+        bottomPanel.add(subPanel);
+        add(bottomPanel,BorderLayout.SOUTH);
+        
+        upperPanel.setLayout(new FlowLayout(FlowLayout.CENTER,30,50));
+        upperPanel.setPreferredSize(new Dimension(500,300));
+        upperPanel.setBackground(new Color(24, 179, 240));
+        upperPanel.add(cloudLabel1);
+        upperPanel.add(label);
+        upperPanel.add(cloudLabel2);
+        add(upperPanel, BorderLayout.NORTH);
+        
         setTitle("HangMan Game");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(500, 500);
