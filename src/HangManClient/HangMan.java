a14f2afb573bf53b22696e02d7b1f6ee3239af4b
Admin <kkrzysztofgargas@gmail.com>
2024-05-08 14:12:28 +0200

    Basic client-server connection implemented. Empty window created

--- src/HangManClient/HangMan.java
+++ src/HangManClient/HangMan.java
@@ -1,12 +1,43 @@
-
 package HangManClient;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import javax.swing.JFrame;
 
-public class HangMan {
+public class HangMan extends JFrame {
 
-    
-    public static void main(String[] args) {
-       
+    public HangMan() {
+        setTitle("HangMan Game");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500, 500);
+        setVisible(true);
     }
-    
+
+    public void connectWithServer() {
+        int port = 6899;
+        String adress = "127.0.0.1";
+        Socket socket = null;
+
+        try {
+            System.out.println("I'm connecting to the server");
+            socket = new Socket(adress, port);
+        } catch (IOException e) {
+            System.out.println("I can't connect to the server");
+        }
+
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String info = in.readLine();
+            System.out.println("I got from server: " + info);
+            socket.close();
+            in.close();
+
+        } catch (IOException e) {
+
+        }
+    }
+
+
 }
--- src/HangManServer/HangManServer.java
+++ src/HangManServer/HangManServer.java
@@ -1,13 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package HangManServer;
 
-/**
- *
- * @author Admin
- */
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class HangManServer {
-    
+
+    private static final int port = 6899;
+    private ServerSocket serverSocket;
+
+    public HangManServer() {
+
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Server is running");
+        } catch (IOException e) {
+            System.out.println("Server can't be initialized");
+            System.exit(0);
+        }
+    }
+
+    public void work() {
+        Socket socket;
+
+        while (true) {
+
+            if (serverSocket == null) {
+                return;
+            }
+
+            try {
+                socket = serverSocket.accept();
+                System.out.println("Client comes: I sent a message");
+
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+                out.println("Działa działa");
+
+                out.close();
+                socket.close();
+                System.out.println("Client left");
+            } catch (IOException e) {
+                System.out.println("issue");
+                System.exit(0);
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+            HangManServer server = new HangManServer();
+            server.work();
+    }
+
 }
